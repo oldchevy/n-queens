22ba07c6edbbc63d45153dad1ca03702b9e00ffe
oldchevy <nick.olszowy@gmail.com>
2016-08-04 11:02:27 -0700

    Naive solution for n-Rooks

--- src/solvers.js
+++ src/solvers.js
@@ -45,122 +45,151 @@ window.findNRooksSolution = function(n) {
 window.countNRooksSolutions = function(n) {
 
 
-
-  // var solutionCount = [];
-
-  // var topLevel = new Board({n: n});
-
-  // var findNextChild = function(board, rooks) {
-
-  //   //debugger;
-
-  //   //var board = new Board(nextboard);
-
-  //   rooks = rooks || 0;
-
-  //   if (rooks === n) {
-
-  //     // var bool = false;
-  //     // _.each(solutionCount, function(unique) {
-  //     //   if (!bool) {
-  //     //     bool = _.isEqual(unique, board.rows());
-  //     //   }
-  //     // });
-
-  //     //if (!bool) {
-  //     var solution = board.rows().slice();
-  //     console.log('Solution: ', solution);
-  //     solutionCount.push(solution);
-  //     //}
-  //   } else {
-
-  //     for (var i = 0; i < n; i++) {
-  //       for (var j = 0; j < n; j++) {
-  //         console.log('Board:', board.rows(), 'Indeces:', i, j, 'Level:', rooks);
-  //         if (!board.get(i)[j]) {
-  //           board.togglePiece(i, j);
-  //           if (board.hasRowConflictAt(i) || board.hasColConflictAt(j)) {
-  //             //board.togglePiece(i, j);
-  //           } else {
-  //             //var childBoard = new Board(board.rows());
-  //             findNextChild(board, rooks + 1);
-  //             console.log('After recursion:', board.rows(), 'Level:', rooks);
-  //             //board.togglePiece(i, j);
-  //           }
-  //         }
-  //       }
-  //     }
-
-      
-  //   }
-  //   //check every point, to see if it's a valid next move
-  //   //if it's a valid next move, recurse on that
-
-  // };
-  // //get all possible combinations of coordinates
-  // //Check each one with hasAnyRooksConflicts
-  // //increment count if it's correct
-
-  // findNextChild(topLevel);
-
-
   // if (n === 1) {
   //   return 1;
   // }
   // if (n === 2) {
   //   return 2;
   // }
+  // // if (n === 3) {
+  // //   return 6;
+  // // }
+  // if (n === 4) {
+  //   return 24;
+  // }
+  // if (n === 5) {
+  //   return 120;
+  // }
+  // if (n === 6) {
+  //   return 720;
+  // }
+  // if (n === 7) {
+  //   return 5040;
+  // }
+  // if (n === 8) {
+  //   return 40320;
+  // }
 
 
+  var solutionCount = [];
+
+  var topLevel = new Board({n: n});
+
+  var findNextChild = function(board, rooks) {
+
+    //debugger;
+
+    //var board = new Board(nextboard);
+
+    rooks = rooks || 0;
+
+    if (rooks === n) {
 
 
-  var board = new Board({n: n});
-
-  var rows = board.rows();
-  // board.togglePiece(rowIndex, colIndex);
-  var findCoords = function() { // finds all available coords based on the current board
-    var coords = [];
-    rows.forEach(function(row, iRow) {
-      row.forEach(function(col, iCol) {
-        board.togglePiece(iRow, iCol);
-        if (!board.hasAnyRowConflicts() && !board.hasAnyColConflicts()) {
-          coords.push([iRow, iCol]);
-        }
-        board.togglePiece(iRow, iCol);
+      var solution = board.rows();
+      var unique = [];
+      board.rows().forEach(function(row){
+        unique.push(row.slice());
       });
-    });
-    return coords;
-  };
+      //console.log('Solution: ', unique);
 
-  var paths = function(board){
-    
-    var tree = Tree(coord);
-    
-    var list = findCoords();
-    list.forEach(function(coord) {
-      tree.addChild(coord);
-    });
+      var bool = false;
+      _.each(solutionCount, function(entry) {
+        if (!bool) {
+          bool = _.isEqual(entry, unique);
+        }
+      });
 
-    
+      if (!bool) {
+        solutionCount.push(unique);
+      }
 
-  };
+    } else {
 
+      for (var i = 0; i < n; i++) {
+        for (var j = 0; j < n; j++) {
+          //console.log('Board:', board.rows(), 'Indeces:', i, j, 'Level:', rooks);
+          if (!board.get(i)[j]) {
+            board.togglePiece(i, j);
+            if (board.hasRowConflictAt(i) || board.hasColConflictAt(j)) {
+              board.togglePiece(i, j);
+            } else {
+              //var childBoard = new Board(board.rows());
+              findNextChild(board, rooks + 1);
+              //console.log('After recursion:', board.rows(), 'Level:', rooks);
+              board.togglePiece(i, j);
+            }
+          }
+        }
+      }
 
-  board.togglePiece(1, 1);
-  console.log(findCoords([1, 1]));
-
-  var addRook = function() {
-    if (arguments.length === n) {
-      // check the coords in arguments to see if it works
-      // if it works, increment 1
+      
     }
-    board.togglePiece(0, 0);
-    var coords = findCoords();
+    //check every point, to see if it's a valid next move
+    //if it's a valid next move, recurse on that
 
+  };
+
+
+  findNextChild(topLevel);
+  
+  console.log('Number of solutions for ' + n + ' rooks:', solutionCount.length);
+  return solutionCount.length;
+
+  //get all possible combinations of coordinates
+  //Check each one with hasAnyRooksConflicts
+  //increment count if it's correct
+
+
+
+
+
+
+  // var board = new Board({n: n});
+
+  // var rows = board.rows();
+  // // board.togglePiece(rowIndex, colIndex);
+  // var findCoords = function() { // finds all available coords based on the current board
+  //   var coords = [];
+  //   rows.forEach(function(row, iRow) {
+  //     row.forEach(function(col, iCol) {
+  //       board.togglePiece(iRow, iCol);
+  //       if (!board.hasAnyRowConflicts() && !board.hasAnyColConflicts()) {
+  //         coords.push([iRow, iCol]);
+  //       }
+  //       board.togglePiece(iRow, iCol);
+  //     });
+  //   });
+  //   return coords;
+  // };
+
+  // var paths = function(board){
+    
+  //   var tree = Tree(coord);
+    
+  //   var list = findCoords();
+  //   list.forEach(function(coord) {
+  //     tree.addChild(coord);
+  //   });
+
+    
 
   // };
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionCount.length;
+
+
+  // board.togglePiece(1, 1);
+  // console.log(findCoords([1, 1]));
+
+  // var addRook = function() {
+  //   if (arguments.length === n) {
+  //     // check the coords in arguments to see if it works
+  //     // if it works, increment 1
+  //   }
+  //   board.togglePiece(0, 0);
+  //   var coords = findCoords();
+
+
+  // // };
 
 };
 
