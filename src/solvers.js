d327141c58864795cdd612342a84e82b1213591d
oldchevy <nick.olszowy@gmail.com>
2016-08-04 12:04:02 -0700

    Working row by row implementation for n-Rooks

--- src/solvers.js
+++ src/solvers.js
@@ -45,103 +45,115 @@ window.findNRooksSolution = function(n) {
 window.countNRooksSolutions = function(n) {
 
 
-  // if (n === 1) {
-  //   return 1;
-  // }
-  // if (n === 2) {
-  //   return 2;
-  // }
-  // // if (n === 3) {
-  // //   return 6;
-  // // }
-  // if (n === 4) {
-  //   return 24;
-  // }
-  // if (n === 5) {
-  //   return 120;
-  // }
-  // if (n === 6) {
-  //   return 720;
-  // }
-  // if (n === 7) {
-  //   return 5040;
-  // }
-  // if (n === 8) {
-  //   return 40320;
-  // }
+  var start = new Date().getTime();
+
+/*********************** ROW BY ROW SOLUTION  *********************************/
+/*                                                                            */ 
+/*     ~4s for n = 8,    ~43s for n = 9                                       */
+
+  var count = 0;
+
+  var board = new Board({n: n});
 
 
-  var solutionCount = [];
+  var rowRecurse = function(row) {
 
-  var topLevel = new Board({n: n});
+    //Initial call, no args are needed.
+    row = row || 0;
 
-  var findNextChild = function(board, rooks) {
+    //Recurse through every row.
+    //Base case is when we go off the board (n)
+    if (row === n) {
 
-    //debugger;
-
-    //var board = new Board(nextboard);
-
-    rooks = rooks || 0;
-
-    if (rooks === n) {
-
-
-      var solution = board.rows();
-      var unique = [];
-      board.rows().forEach(function(row){
-        unique.push(row.slice());
-      });
-      //console.log('Solution: ', unique);
-
-      var bool = false;
-      _.each(solutionCount, function(entry) {
-        if (!bool) {
-          bool = _.isEqual(entry, unique);
-        }
-      });
-
-      if (!bool) {
-        solutionCount.push(unique);
-      }
+      count++;
+      return;
 
     } else {
 
-      for (var i = 0; i < n; i++) {
-        for (var j = 0; j < n; j++) {
-          //console.log('Board:', board.rows(), 'Indeces:', i, j, 'Level:', rooks);
-          if (!board.get(i)[j]) {
-            board.togglePiece(i, j);
-            if (board.hasRowConflictAt(i) || board.hasColConflictAt(j)) {
-              board.togglePiece(i, j);
-            } else {
-              //var childBoard = new Board(board.rows());
-              findNextChild(board, rooks + 1);
-              //console.log('After recursion:', board.rows(), 'Level:', rooks);
-              board.togglePiece(i, j);
-            }
-          }
+      for (var j = 0; j < n; j++) {
+
+        //At every row, we toggle a piece onto every column position
+        board.togglePiece(row, j);
+  
+        //If conflict, toggle back off
+        //No need to check rows now!
+        if (board.hasColConflictAt(j)) {
+          board.togglePiece(row, j);
+        } else {
+        //If no conflict, recurse down to the next row
+        
+          rowRecurse(row + 1);
+          //Toggle piece back off to move on to the next column position in the working row
+          board.togglePiece(row, j);
         }
       }
-
-      
     }
-    //check every point, to see if it's a valid next move
-    //if it's a valid next move, recurse on that
-
   };
 
-
-  findNextChild(topLevel);
+  rowRecurse(0);
   
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount.length);
-  return solutionCount.length;
+  var end = new Date().getTime();
+  console.log('Row by row method for ' + n + ' rooks: ', count);
+  console.log('Time taken: ', (end - start) / 1000);
+  return count;
 
-  //get all possible combinations of coordinates
-  //Check each one with hasAnyRooksConflicts
-  //increment count if it's correct
+/*********************** NAIVE SOLUTION  *********************************/
+/*                                                                       */ 
+/* This solution doesn't do well past n=5                                */
 
+  // var solutionCount = [];
+  // var findNextChild = function(board, rooks) {
 
+  //   //Top level assignment
+  //   rooks = rooks || 0;
 
+  //   //Base case
+  //   if (rooks === n) {
+
+  //     //Take a deep slice copy (the board is a reference, and so is each row array inside the board!!!)
+  //     var solution = board.rows();
+  //     var unique = [];
+  //     board.rows().forEach(function(row) {
+  //       unique.push(row.slice());
+  //     });
+
+  //     //Check every board already in solutions to see if it's already there
+  //     //With a deep equal comparison
+  //     var bool = false;
+  //     _.each(solutionCount, function(entry) {
+  //       if (!bool) {
+  //         bool = _.isEqual(entry, unique);
+  //       }
+  //     });
+
+  //     //If unique solution, push it into the solution array
+  //     if (!bool) {
+  //       solutionCount.push(unique);
+  //     }
+
+  //   } else {
+  //     //Not base case
+  //     for (var i = 0; i < n; i++) {
+  //       for (var j = 0; j < n; j++) {
+  //         //Checking every position at every level, not needed!
+  //         if (!board.get(i)[j]) {
+  //           //Only toggle when there's not already a piece
+  //           board.togglePiece(i, j);
+  //           if (board.hasRowConflictAt(i) || board.hasColConflictAt(j)) {
+  //             //If there's a conflict, toggle back off and continue
+  //             board.togglePiece(i, j);
+  //           } else {
+  //             //recursive case
+  //             findNextChild(board, rooks + 1);
+  //             //Toggle back off
+  //             board.togglePiece(i, j);
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+
+  // };
 
 
 
